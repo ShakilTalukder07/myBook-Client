074c7ea83a8c8cce89f35f9db55bd7c00bd4d353
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-12-28 20:31:54 +0600

    love react add

--- src/pages/Media/Post.js
+++ src/pages/Media/Post.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FiHeart, FiMessageSquare } from "react-icons/fi";
+import './Post.css'
 
-const Post = ({singlePost}) => {
+const Post = ({ singlePost }) => {
 
-    const  { _id, image, name} = singlePost
+    const [btnColor, setBtnColor] = useState(false)
+
+    const { _id, image, name } = singlePost
+
+    function handleClick(){
+        setBtnColor( btnColor => !btnColor)
+    }
+
+    let toggleClassCheck = btnColor ? ' active' : ""
 
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img className='h-64 w-full' src={image} alt="Shoes" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <div className="card-actions justify-end">
-                <Link to={`/allPost/${_id}`} className="btn btn-primary">View details</Link>
+                <div className="card-actions ">
+                    <Link onClick={handleClick}> <FiHeart className={`heart${toggleClassCheck}`}></FiHeart> </Link>
+                    <FiMessageSquare className='comment'></FiMessageSquare>
+                    <Link to={`/allPost/${_id}`} className="btn h-6 ml-36 ">View details</Link>
                 </div>
             </div>
         </div>
